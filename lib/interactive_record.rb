b18953152d6e64f28a4485138ce2be73438e0c91
Micah Banschick <micha.ban@gmail.com>
2020-04-06 18:03:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
   end 
   
   def self.find_by(attribute)
-    sql = attribute.each{|key, value| "#{selecting} WHERE #{key} = #{value}"}
+    sql = attribute.map{|key, value| "#{selecting} WHERE #{key} = #{value}"}[0]
     DB[:conn].execute(sql)
   end 
   
